097258bfb87fc0479963357353fe9169c047dc32
Ari-Ario <xusrew.z@gmail.com>
2026-03-27 19:51:21 +0100

    Final fix for import.meta and white screen

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,17 +1,18 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            'babel-plugin-transform-import-meta',
-            'react-native-reanimated/plugin', // Required for Reanimated
-        ],
-        overrides: [
-            {
-                test: /\.(js|mjs|ts|tsx)$/,
-                include: /[\\/]node_modules[\\/]/,
-                plugins: ['babel-plugin-transform-import-meta'],
-            },
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'babel-plugin-transform-import-meta',
+      'react-native-reanimated/plugin',
+    ],
+    overrides: [
+      {
+        test: /\.[jt]sx?$/,
+        include: /[\\/]node_modules[\\/]/,
+        plugins: ['babel-plugin-transform-import-meta'],
+      },
+    ],
+  };
 };
+
