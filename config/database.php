da84df3023d451c6766954408d71f570e9b73921
Ari-Ario <xusrew.z@gmail.com>
2026-08-23 15:23:00 +0200

    Configure phpredis read_timeout and timeout in config/database.php to fix Redis connection read errors under Octane

--- config/database.php
+++ config/database.php
@@ -168,6 +168,8 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'timeout' => 5.0,
+            'read_timeout' => 60.0,
         ],
 
         'cache' => [
@@ -177,6 +179,8 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'timeout' => 5.0,
+            'read_timeout' => 60.0,
         ],
 
     ],
